834d087dfee41994680c149352fd6d7cf7d23844
soypat <graded.sp@gmail.com>
2022-03-26 23:08:18 -0300

    Color implements color.Color interface

--- math_color.go
+++ math_color.go
@@ -46,3 +46,34 @@ func NewColorHex(i int64) *Color {
 		Value: three.Get("Color").New(i),
 	}
 }
+
+// RGBA returns the alpha-premultiplied red, green, blue and alpha values
+// for the color. Each value ranges within [0, 0xffff], but is represented
+// by a uint32 so that multiplying by a blend factor up to 0xffff will not
+// overflow.
+//
+// An alpha-premultiplied color component c has been scaled by alpha (a),
+// so has valid values 0 <= c <= a.
+func (c *Color) RGBA() (r, g, b, a uint32) {
+	h := c.GetHex()
+	// See color.RGBA{} algorithm.
+	r = uint32((h & 0xff0000) >> 16)
+	r |= r << 8
+	g = uint32((h & 0x00ff00) >> 8)
+	g |= g << 8
+	b = uint32(h & 0x0000ff)
+	b |= b << 8
+	return r, g, b, 0xffff
+}
+
+func (c *Color) GetHex() int {
+	return c.Call("getHex").Int()
+}
+
+func (c *Color) GetByteHex() (r, g, b uint8) {
+	h := c.GetHex()
+	r = uint8((h & 0xff0000) >> 16)
+	g = uint8((h & 0x00ff00) >> 8)
+	b = uint8(b & 0x0000ff)
+	return r, g, b
+}
